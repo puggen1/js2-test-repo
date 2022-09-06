6e1b5264d6aa1f47eed56af0ef90c1c4c550be1b
puggen1 <43915072+puggen1@users.noreply.github.com>
2022-09-06 15:55:59 +0200

    added fetch function, display data function

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-/** my @class  that does something*/
-class myClass{
- /**
-  * This class does something that should be written here
-  * @author Bendik
-  * 
-  * 
-  */
+const cors ="https://noroffcors.herokuapp.com/";
+let url="https://www.geek.no/noroff/stuff/restauranter.json";
+
+let htmlList = document.querySelector("#myList");
+
+
+
+
+async function getData(prefix, data){
+  await fetch(prefix + data)
+  .then(function(response){
+    console.log(response);
+    return response.json();
+  })
+  .then(function(formatedResponse){
+    console.log(formatedResponse);
+    showData(formatedResponse, htmlList);
+  })
+  .catch(error => console.error(error))
 }
 
+getData(cors, url);
 
-console.log(":)");
-
-
-console.log(":(");
+/**
+ * 
+ * @param {object} data the Data you get from the api
+ * @param {object} htmlTarget the place you want the data to be listed to
+ */
+function showData(data, htmlTarget){
+  let html = ""
+  console.log(data);
+  console.log(htmlTarget);
+  for(restaurant of data.restaurants){
+    console.log(restaurant);
+    html += `<li> <h2>${restaurant.name}</h2> ${restaurant.branch} ${restaurant.address} <a href="${restaurant.url}">Link to webpage</a> </li>`
+  }
+  htmlList.innerHTML += html;
+}
