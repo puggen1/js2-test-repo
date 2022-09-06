8c6ea406962861c221765f2e605f76427cd3f9ae
puggen1 <43915072+puggen1@users.noreply.github.com>
2022-09-06 20:11:37 +0200

    added example to jsdocs

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-const cors ="https://noroffcors.herokuapp.com/";
-let url="https://www.geek.no/noroff/stuff/restauranter.json";
+const cors = "https://noroffcors.herokuapp.com/";
+let url = "https://www.geek.no/noroff/stuff/restauranter.json";
 
 let htmlList = document.querySelector("#myList");
 
-
-
-
-async function getData(prefix, data){
+async function getData(prefix, data) {
   await fetch(prefix + data)
-  .then(function(response){
-    console.log(response);
-    return response.json();
-  })
-  .then(function(formatedResponse){
-    console.log(formatedResponse);
-    showData(formatedResponse, htmlList);
-  })
-  .catch(error => console.error(error))
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (formatedResponse) {
+      console.log(formatedResponse);
+      showData(formatedResponse, htmlList);
+    })
+    .catch((error) => console.error(error));
 }
 
 getData(cors, url);
 
 /**
- * 
+ *
  * @param {object} data the Data you get from the api
  * @param {object} htmlTarget the place you want the data to be listed to
+ * @example
+ * showData(someData, TargetHtml)
  */
-function showData(data, htmlTarget){
-  let html = ""
+function showData(data, htmlTarget) {
+  let html = "";
   console.log(data);
   console.log(htmlTarget);
-  for(restaurant of data.restaurants){
+  for (restaurant of data.restaurants) {
     console.log(restaurant);
-    html += `<li> <h2>${restaurant.name}</h2> ${restaurant.branch} ${restaurant.address} <a href="${restaurant.url}">Link to webpage</a> </li>`
+    html += `<li> <h2>${restaurant.name}</h2> ${restaurant.branch} ${restaurant.address} <a href="${restaurant.url}">Link to webpage</a> </li>`;
   }
   htmlList.innerHTML += html;
 }
