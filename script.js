256854e6822407d59c8d6fa4ec4f099c895ce289
puggen1 <43915072+puggen1@users.noreply.github.com>
2022-09-06 21:58:36 +0200

    jsdocs files

--- script.js
+++ script.js
@@ -19,19 +19,35 @@ async function getData(prefix, data) {
 getData(cors, url);
 
 /**
- *
+ * Function to show content on html list
+ * not universal, but might get updated
  * @param {object} data the Data you get from the api
  * @param {object} htmlTarget the place you want the data to be listed to
  * @example
- * showData(someData, TargetHtml)
+ * //add some content and location
+ * showData(someData, htmlLocation)
+ * // input will be looped and output consists of <li> elements
  */
 function showData(data, htmlTarget) {
   let html = "";
+
   console.log(data);
   console.log(htmlTarget);
   for (restaurant of data.restaurants) {
+    let branch = "";
+    let address = "";
+    let url = "";
+    if (restaurant.branch && !null) {
+      branch = restaurant.branch;
+    }
+    if (restaurant.address && !null) {
+      address = restaurant.address;
+    }
+    if (restaurant.url && !null) {
+      url = `<a href="${restaurant.url}">Website </a>`;
+    }
     console.log(restaurant);
-    html += `<li> <h2>${restaurant.name}</h2> ${restaurant.branch} ${restaurant.address} <a href="${restaurant.url}">Link to webpage</a> </li>`;
+    html += `<li> <h2>${restaurant.name}</h2> ${branch} ${address} ${url}  </li>`;
   }
   htmlList.innerHTML += html;
 }
---
+++ test.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
+
+function myFunction() {
+  console.log("this is a function");
+}
+
+function helloWorld() {
+  return "hello world";
+}
+
+myFunction();
