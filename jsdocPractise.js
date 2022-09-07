962b9b6f9439f7fd46967ea48b8d0e381482e1fd
puggen1 <43915072+puggen1@users.noreply.github.com>
2022-09-07 13:52:57 +0200

    more js practise

---
+++ jsdocPractise.js
@@ -0,0 +1,57 @@
+/**
+ * adds two numbers together and returns them
+ * @param {number} a first number to add
+ * @param {number} b second number to add
+ * @returns {number} the two numbers now added together to one
+ * @example
+ * ```js
+ * //give it two numbers
+ * let finalNumber = addNumbers(10, 5)
+ * console.log(finalNumber)
+ * //logs to console: 15 
+ * ```
+ */
+
+
+function addNumbers(a, b) {
+    const result = a + b;
+    return result;
+  }
+
+
+
+
+  const person = {
+    name :"Kylar",
+    age:21,
+    job:"stipp",
+    city:"Lofthus"
+  };
+  console.log(typeof(person));
+  console.log(person);
+
+  let stringyfiedPerson = JSON.stringify(person);
+  console.log(typeof(stringyfiedPerson));
+  console.log(stringyfiedPerson);
+
+  let returenedPerson =JSON.parse(stringyfiedPerson);
+  console.log(typeof(returenedPerson));
+  console.log(returenedPerson);
+
+/*
+  const firstName = localStorage.getItem("firstName");
+  console.log(firstName);
+  localStorage.removeItem("firstName");
+  console.log(localStorage.getItem("firstName"));*/
+
+  
+
+  const pet = {
+    type: 'Dog',
+    name: 'Lilje',
+  };
+
+  const retrivedPet = localStorage.getItem("pet");
+  console.log(retrivedPet);
+  let formatedPet = JSON.parse(retrivedPet);
+  console.log(formatedPet)
--- script.js
+++ script.js
@@ -3,7 +3,23 @@ let url = "https://www.geek.no/noroff/stuff/restauranter.json";
 
 let htmlList = document.querySelector("#myList");
 
-async function getData(prefix, data) {
+
+
+/**
+ * used to fetch api and send to another function
+ * @param {string} [prefix] optional cors to access api
+ * @param {string} data link to api
+ * @example
+ * ```js
+ * //to get the wanted api
+ * const cors = "https://noroffcors.herokuapp.com/";
+ * let url = "https://www.geek.no/noroff/stuff/restauranter.json";
+ * getData(cors, url);
+ * // function will get the api and send it to
+ * showData(formatedResponse, targetHtml)
+ * ```
+ */
+async function getData(prefix="", data) {
   await fetch(prefix + data)
     .then(function (response) {
       console.log(response);
@@ -16,6 +32,36 @@ async function getData(prefix, data) {
     .catch((error) => console.error(error));
 }
 
+/**
+ * used to get data from an object, so it is easy to access later
+ * @param {object} data a list of restaurants and info about them
+ * @returns {object} object of variables and html
+ * @example 
+ * ```js
+ * //when you want to get access to data and not write the whole json object
+ * let formatedData = formatData(list.restaurant[i])
+ * returns ["bella napoli", "nesttun", "nesttunvegen 1", "<a href="www.bellanapoli.no">Website</a>"]
+ * ```
+ */
+
+function formatData(data){
+    let branch = "";
+    let address = "";
+    let url = "";
+    if (data.branch && !null) {
+      branch = data.branch;
+    }
+    if (data.address && !null) {
+      address = data.address;
+    }
+    if (data.url && !null) {
+      url = `<a href="${data.url}">Website </a>`;
+    }
+    console.log(data);
+    console.log(data.name, branch,address,url)
+    return [data.name, branch, address, url]
+}
+
 getData(cors, url);
 
 /**
@@ -24,30 +70,20 @@ getData(cors, url);
  * @param {object} data the Data you get from the api
  * @param {object} htmlTarget the place you want the data to be listed to
  * @example
+ * ```js
+ * //used to display specific content in a list
  * //add some content and location
  * showData(someData, htmlLocation)
- * // input will be looped and output consists of <li> elements
+ * //adds listitems to htmlTarget (ul or ol)
+ * ```
  */
 function showData(data, htmlTarget) {
+  let returnedData = []
   let html = "";
-
-  console.log(data);
+  for(let i = 0; i < data.restaurants.length; i++){
+  returnedData = formatData(data.restaurants[i])
+  console.log(returnedData);
   console.log(htmlTarget);
-  for (restaurant of data.restaurants) {
-    let branch = "";
-    let address = "";
-    let url = "";
-    if (restaurant.branch && !null) {
-      branch = restaurant.branch;
-    }
-    if (restaurant.address && !null) {
-      address = restaurant.address;
-    }
-    if (restaurant.url && !null) {
-      url = `<a href="${restaurant.url}">Website </a>`;
-    }
-    console.log(restaurant);
-    html += `<li> <h2>${restaurant.name}</h2> ${branch} ${address} ${url}  </li>`;
-  }
-  htmlList.innerHTML += html;
-}
+  html += `<li> <h2>${returnedData[0]}</h2> ${returnedData[1]} ${returnedData[2]} ${returnedData[3]}  </li>`;
+ }  
+  htmlList.innerHTML += html;}
